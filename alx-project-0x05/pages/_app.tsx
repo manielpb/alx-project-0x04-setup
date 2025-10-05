5104a5120c93a73e364bc9f653f54a8ce74339b9
maniel <manuelpepr99@icloud.com>
2025-10-05 17:40:51 +0000

    Include a ContextAPI for global state management

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
-import { CountProvider } from "@/context/CountContent";
+import { CountProvider } from "@/context/CountContext";
 
 export default function App({
   Component, pageProps
